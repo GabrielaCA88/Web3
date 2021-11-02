8bffd93232daab4ccb60f14fce06a7c59c7fa496
GABRIELA CASTILLO ARECO <gabriela.castillo.areco@gmail.com>
2021-11-02 17:26:40 +0100

    searches the id of the latest 10 blocks

---
+++ searching_blocks.py
@@ -0,0 +1,14 @@
+from web3 import Web3
+
+infura_url= 'https://mainnet.infura.io/v3/c94afc5937c843b39a39d595881c5dc1'
+web3= Web3(Web3.HTTPProvider (infura_url))
+
+# get latest 10 blocks
+blocks_id=[]
+latest = web3.eth.blockNumber
+for i in range(0, 10):
+  blocks= (web3.eth.getBlock(latest - i))
+  #print(web3.toHex(blocks.hash))
+  blocks_id.append (web3.toHex(blocks.hash))
+
+print (blocks_id)
